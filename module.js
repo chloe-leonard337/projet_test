4774f4ef77b5b101d6600fcc1d3328a92f1a388f
LEONARD Chloé <cleonard@terrena.fr>
2026-02-06 15:16:19 +0100

    Tests unitaires (codePostal, identite, mail)

--- module.js
+++ module.js
@@ -6,8 +6,27 @@
  */
 
 function calculateAge(p) {
+    if (p === undefined) {
+        throw new Error('missing param p');
+    }
+    if (typeof p !== 'object' || p === null) {
+        throw new Error('param p should be an object');
+    }
+    if (!p.hasOwnProperty('birth')) {
+        throw new Error('object p should have a birth parameter');
+    }
+    if (!(p.birth instanceof Date)) {
+        throw new Error('birth parameter should be a Date object');
+    }
+    if (isNaN(p.birth.getTime())) {
+        throw new Error('birth date is invalid');
+    }
+    if (p.birth.getTime() > Date.now()) {
+        throw new Error('birth date is in the future');
+    }
     let DateDiff = new Date(Date.now() - p.birth.getTime());
     let age = Math.abs(DateDiff.getUTCFullYear() - 1970);
     return age;
 }
 
+export { calculateAge };
