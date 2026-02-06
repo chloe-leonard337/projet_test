d88aac05db994b4ff48be1b7adbef75528c5341c
LEONARD Chloé <cleonard@terrena.fr>
2026-02-06 15:18:50 +0100

    developpement des fonctions pour code postal, identite et mail

--- module.js
+++ module.js
@@ -29,4 +29,68 @@ function calculateAge(p) {
     return age;
 }
 
-export { calculateAge };
+/**
+ * 
+ * @param {number} postalCode 
+ * @returns {boolean} true if postal code is valid, false otherwise
+ * 
+ */
+function validatePostalCode(postalCode) {
+    if (!postalCode || postalCode.length === 0) {
+        throw new Error('postal code should not be empty');
+    }
+    if (postalCode.length !== 5) {
+        throw new Error('postal code should have 5 digits');
+    }
+    if (!/^\d{5}$/.test(postalCode)) {
+        throw new Error('postal code should be a 5-digit number');
+    }
+    return true;
+}
+
+
+
+function verifyIdentity(identity) {
+    // Le paramètre identity doit être défini
+    if (identity === undefined) {
+        throw new Error('missing param identity');
+    }
+    // Le paramètre identity doit être un objet non null
+    if (typeof identity !== 'object' || identity === null) {
+        throw new Error('param identity should be an object');
+    }
+    // Le paramètre name doit être présent
+    if (!identity.hasOwnProperty('name')) {
+        throw new Error('object identity should have a name parameter');
+    }
+    // Le nom doit être une chaîne de caractères
+    if (typeof identity.name !== 'string') {
+        throw new Error('name parameter should be a string');
+    }
+    //protection contre les attaques XSS
+    if (identity.name.match(/<script.*?>.*?<\/script>/i)) {
+        throw new Error('name parameter should not contain script tags');
+    }
+    // ni chiffre ni caractère spécial (sauf exceptions)
+    if (identity.name.match(/[^a-zA-ZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÜÝàáâãäåæçèéêëìíîïñòóôõöøùúûüýÿ\s'-]/)) {
+        throw new Error('name parameter should not contain numbers or special characters');
+    }
+    return true;
+}
+
+
+function verifyEmail(email) {
+    if (email === undefined) {
+        throw new Error('missing param email');
+    }
+    if (typeof email !== 'string') {
+        throw new Error('param email should be a string');
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        throw new Error('email parameter should be a valid email address');
+    }
+    return true;
+}
+
+export { calculateAge, validatePostalCode, verifyIdentity, verifyEmail };
